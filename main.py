28b7424f97b6f8f9508b5e36c44872870dffb510
马腾骁 <45908717+dustin-ma@users.noreply.github.com>
2022-04-20 19:01:27 -0400

    Added simple bot commands

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import discord
 import os
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     print('[' + message.channel.name + '] ' + message.author.name + ': ' + message.content)
+#     if message.author == client.user:
+#         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
 
-print(os.getenv('BOT_TOKEN'))
+@client.command()
+async def ping(ctx):
+    await ctx.send('No!')
+
 client.run(os.getenv('BOT_TOKEN'))
